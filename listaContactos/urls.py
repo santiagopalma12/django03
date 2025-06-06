756413129917f1c7901a571666fbc81bc76d7a77
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-06-05 23:30:37 -0500

    Crea vista, template y url para renderizar al objeto persona, paso también el objeto Persona completo al contexto

--- listaContactos/urls.py
+++ listaContactos/urls.py
@@ -1,9 +1,11 @@
+# tu_proyecto/urls.py
 from django.contrib import admin
 from django.urls import path
-from inicio import views
+from inicio.views import myHomeView
+from personas.views import personaTestView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
-    path('another/', views.another),
+    path('', myHomeView, name='home'),
+    path('persona/', personaTestView, name='test-persona'), 
 ]
--- personas/views.py
+++ personas/views.py
@@ -1,3 +1,10 @@
+# personas/views.py
 from django.shortcuts import render
+from .models import Persona
 
-# Create your views here.
+def personaTestView(request):
+    obj = Persona.objects.get(id=1)
+    context = {
+        'objeto': obj
+    }
+    return render(request, "personas/descripcion.html", context)
