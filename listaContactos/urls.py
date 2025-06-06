db09c210551516e76c222ba295272620e286f11b
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-06-05 23:33:42 -0500

    Crea template y URL para la creación de Personas

--- listaContactos/urls.py
+++ listaContactos/urls.py
@@ -2,10 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 from inicio.views import myHomeView
-from personas.views import personaTestView 
+from personas.views import personaTestView, personaCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', myHomeView, name='home'),
     path('persona/', personaTestView, name='test-persona'), 
+    path('persona/', personaTestView, name='test-persona'),
+    path('agregar/', personaCreateView, name='create-persona'),
 ]
