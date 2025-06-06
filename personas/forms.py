899fff7d7d5fcac096d3d6ba93c3cbf4364dc42d
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-06-05 23:32:20 -0500

    Crea ModelForm PersonaForm y vista personaCreateView para manejarlo

---
+++ personas/forms.py
@@ -0,0 +1,13 @@
+# personas/forms.py
+from django import forms
+from .models import Persona
+
+class PersonaForm(forms.ModelForm):
+    class Meta:
+        model = Persona
+        fields = [
+            'nombres',
+            'apellidos',
+            'edad',
+            'donador',
+        ]
--- personas/views.py
+++ personas/views.py
@@ -1,6 +1,7 @@
 # personas/views.py
 from django.shortcuts import render
 from .models import Persona
+from .forms import PersonaForm
 
 def personaTestView(request):
     obj = Persona.objects.get(id=1)
@@ -8,3 +9,14 @@ def personaTestView(request):
         'objeto': obj
     }
     return render(request, "personas/descripcion.html", context)
+
+def personaCreateView(request):
+    form = PersonaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = PersonaForm() 
+    
+    context = {
+        'form': form
+    }
+    return render(request, "personas/personasCreate.html", context)
