c010696abd8165ab21710469e238f50540fbe55a
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-06-28 17:11:08 -0500

    ctualizar personas/urls.py

--- personas/urls.py
+++ personas/urls.py
@@ -1,9 +1,10 @@
 # personas/urls.py
 from django.urls import path
-from .views import PersonaListView
+from .views import PersonaListView, PersonaDetailView
 
 app_name = 'personas'
 
 urlpatterns = [
     path('', PersonaListView.as_view(), name='persona-list'),
+    path('<int:pk>/', PersonaDetailView.as_view(), name='persona-detail'),
 ]
