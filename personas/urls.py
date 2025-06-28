7ead1c80280dd7fc44bdcbf492c9e8a3f354b23f
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-06-28 17:28:02 -0500

    Actualizar personas/urls.py

--- personas/urls.py
+++ personas/urls.py
@@ -1,10 +1,14 @@
 # personas/urls.py
-from django.urls import path
-from .views import PersonaListView, PersonaDetailView
-
-app_name = 'personas'
+from .views import (
+    PersonaListView,
+    PersonaDetailView,
+    PersonaCreateView,
+    PersonaUpdateView
+)
 
 urlpatterns = [
     path('', PersonaListView.as_view(), name='persona-list'),
+    path('create/', PersonaCreateView.as_view(), name='persona-create'),
     path('<int:pk>/', PersonaDetailView.as_view(), name='persona-detail'),
+    path('<int:pk>/update/', PersonaUpdateView.as_view(), name='persona-update'),
 ]
