6e5b0812c14577334f6f48cc83960f53ee4eccb1
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-06-28 17:03:41 -0500

    feat(personas): Implementar ListView para listar personas

--- personas/views.py
+++ personas/views.py
@@ -1,27 +1,12 @@
 # personas/views.py
-from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Persona
-from .forms import PersonaForm
 
-def personaTestView(request):
-    obj = Persona.objects.get(id=1)
-    context = {
-        'objeto': obj
-    }
-    return render(request, "personas/descripcion.html", context)
-
-def personaCreateView(request):
-    if request.method == 'POST':
-        form = PersonaForm(request.POST)
-        if form.is_valid():
-            form.save()
-            form = PersonaForm()
-    else: 
-        form = PersonaForm()
-    
-    context = { 'form': form }
-    return render(request, "personas/personasCreate.html", context)
-
-def searchForHelp(request):
-    return render(request, "personas/search.html")
-
+class PersonaListView(ListView):
+    model = Persona
