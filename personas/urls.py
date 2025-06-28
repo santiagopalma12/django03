1f3c093ae22e8f0c3af01d2d83b74f83287dbc4f
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-06-28 17:34:01 -0500

    Añadir vista a personas/views.py

--- personas/urls.py
+++ personas/urls.py
@@ -1,11 +1,12 @@
 # personas/urls.py
+from django.urls import path
 from .views import (
     PersonaListView,
     PersonaDetailView,
     PersonaCreateView,
     PersonaUpdateView
 )
-
+app_name = 'personas'
 urlpatterns = [
     path('', PersonaListView.as_view(), name='persona-list'),
     path('create/', PersonaCreateView.as_view(), name='persona-create'),
--- personas/views.py
+++ personas/views.py
@@ -1,11 +1,6 @@
 # personas/views.py
-from django.views.generic import (
-    ListView,
-    DetailView,
-    CreateView,
-    UpdateView,
-    DeleteView
-)
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView 
+from django.urls import reverse_lazy
 from .models import Persona
 
 class PersonaListView(ListView):
@@ -14,3 +9,6 @@ class PersonaListView(ListView):
 class PersonaDetailView(DetailView):
     fields = ['nombres', 'apellidos', 'edad', 'donador']
     model = Persona
+class PersonaDeleteView(DeleteView):
+    model = Persona
+    success_url = reverse_lazy('personas:persona-list')
